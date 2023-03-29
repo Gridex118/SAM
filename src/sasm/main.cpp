545e34610668a353d7da063a3b59a1bcb9584707
Alex Rosegrid <rosegrid58@gmail.com>
2023-03-29 16:06:23 +0530

    .

--- src/sasm/main.cpp
+++ src/sasm/main.cpp
@@ -1,7 +1,19 @@
+#include <iostream>
 #include "assembler.hpp"
 
-int main(int argc, char **argv) {
-    assemble::SamAssembler assembler(argv[1], argv[2]);
-    assembler.assemble();
+int main(int argc, char **argv){
+    if ((argc != 2) && (argc != 3)) {
+        std::cout << "Syntax: sasm SOURCE [SINK] \n";
+    } else {
+        char *sink_file;
+        if (argc == 2)
+            sink_file = "a.sam";
+        else
+            sink_file = argv[2];
+        assemble::SamAssembler assembler(argv[1], sink_file);
+        if (assembler.assemble() == -1) {
+            return -1;
+        }
+    }
     return 0;
 }
